97b21c570d1dea119f77c10c05f02dfdf16a7b26
qianxk <147289951@qq.com>
2017-05-12 13:41:14 +0800

    修正个人用户中心，活动开始时间两天的判断时间参照为场次的开始时间；添加个人用户中心取消活动时发送短信处理

--- web-admin/src/main/java/com/creatoo/hn/actions/home/userCenter/UserCenterAction.java
+++ web-admin/src/main/java/com/creatoo/hn/actions/home/userCenter/UserCenterAction.java
@@ -1,12 +1,11 @@
 package com.creatoo.hn.actions.home.userCenter;
 
 import com.creatoo.hn.ext.bean.ResponseBean;
-import com.creatoo.hn.model.WhCode;
-import com.creatoo.hn.model.WhUser;
-import com.creatoo.hn.model.WhUserAlerts;
-import com.creatoo.hn.model.WhgActOrder;
+import com.creatoo.hn.model.*;
 import com.creatoo.hn.services.comm.CommService;
+import com.creatoo.hn.services.comm.SMSService;
 import com.creatoo.hn.services.home.agdindex.IndexPageService;
+import com.creatoo.hn.services.home.agdwhhd.WhhdService;
 import com.creatoo.hn.services.home.userCenter.UserCenterService;
 import com.creatoo.hn.utils.CommUtil;
 import com.creatoo.hn.utils.WhConstance;
@@ -49,6 +48,13 @@ public class UserCenterAction {
 	
 	@Autowired
 	private IndexPageService indexService;
+
+    @Autowired
+    private WhhdService WhhdService;
+
+    @Autowired
+    private SMSService smsService;
+
 	/**
 	 * 用户名登录验证
 	 * 
@@ -752,8 +758,20 @@ public class UserCenterAction {
 		actOrder.setTicketstatus(3);
 		actOrder.setOrderisvalid(2);
 		userCenterService.upActOrder(actOrder);
-		return res;
-		
+
+        //发送取消短信
+        try {
+            WhgActActivity act = WhhdService.getActDetail(actOrder.getActivityid());
+            Map<String, String> smsData = new HashMap<String, String>();
+            smsData.put("userName", actOrder.getOrdername());
+            smsData.put("activityName", act.getName());
+            smsData.put("ordernumber", actOrder.getOrdernumber());
+            smsService.t_sendSMS(actOrder.getOrderphoneno(), "ACT_UNORDER", smsData);
+        } catch (Exception e) {
+            log.error("取消活动订单短信发送失败", e);
+        }
+
+        return res;
 	}
 
 	/**
